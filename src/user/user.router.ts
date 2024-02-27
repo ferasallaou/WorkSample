cbadffe20309ffca6278c4b5d5605c81d9183fe2
Feras Allaou <feras@Ferass-MacBook-Pro.local>
2024-02-27 17:55:03 +0000

    add eslint

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,10 +1,9 @@
-import express from 'express'
-import { createUser, getUsers } from './user.service'
-import { validateUser } from './dto'
-import {query} from 'express-validator'
+import express from 'express';
+import { query } from 'express-validator';
+import { createUser, getUsers } from './user.service';
+import { validateUser } from './dto';
 
-export const Router = express.Router()
+export const Router = express.Router();
 
-
-Router.get('/', query('created').default('desc') ,getUsers)
-Router.post('/', validateUser, createUser)
+Router.get('/', query('created').default('desc'), getUsers);
+Router.post('/', validateUser, createUser);
